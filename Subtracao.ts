e632044eebfa9edb9e0178b3b1d9cecaae5fea1e
Gabi <168978076+GabSantt@users.noreply.github.com>
2026-03-09 16:40:59 -0300

    Ajustes
    Added a comment to the calcular method.

--- Subtracao.ts
+++ Subtracao.ts
@@ -2,6 +2,6 @@ import Calculo from "./Calculo"
 
 export default class Subtracao extends Calculo {
   public calcular(): number {
-    return this.numero1 - this.numero2
+    return this.numero1 - this.numero2 /*menosémaisachoeu*/
   }
 }
